4e5ac97d52ffaa9432f55bb71bc90db1eb1fbcd1
Tanmay Jana <tanmay.jana@mountblue.io>
2020-05-27 17:36:06 +0530

    added section footer

--- src/Components/Home/Footer/Footer.js
+++ src/Components/Home/Footer/Footer.js
@@ -1,12 +1,54 @@
-import React, { Component } from 'react'
-import './Footer.class'
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React, { Component } from "react";
+import "./Footer.css";
 
 export default class Footer extends Component {
-    render() {
-        return (
-            <div className = "footer-container">
-                
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="footer-container">
+        <div className="graviky-block">
+          <h4>GRAVIKY LABS</h4>
+          <p>
+            We help business around the globe become carbon negative and push
+            frontiers in sustainablity.
+          </p>
+          <p id="copyright">© 2020 Graviky Labs</p>
+        </div>
+        <div className="footer-links-block">
+          <a href="#" className="footer-link">
+            PRODUCTS
+          </a>
+          <a href="#" className="footer-link">
+            PARTNERSHIP
+          </a>
+          <a href="#" className="footer-link">
+            COMMUNITY
+          </a>
+          <a href="#" className="footer-link">
+            CONTACT
+          </a>
+        </div>
+        <div className="footer-social-block">
+          <h4>FOLLOW US</h4>
+          <div className="footer-icon-block">
+            <a>
+              <i className="fa fa-facebook"></i>
+            </a>
+            <a>
+              <i className="fa fa-twitter"></i>
+            </a>
+            <a>
+              <i className="fa fa-instagram"></i>
+            </a>
+          </div>
+        </div>
+        <div className="contacts-block">
+          <h4>CONTACT US</h4>
+          <p>The Castle Unit 345 2500 Castle Manhattan, New York City</p>
+          <p>T: +216 (0)40 3629 4753</p>
+          <p>E: hello@themenectar.com</p>
+        </div>
+      </div>
+    );
+  }
 }
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -9,6 +9,7 @@ import Section6 from './Section6/Section6';
 import Section7 from './Section7/Section7';
 import Section8 from './Section8/Section8';
 import Section9 from './Section9/Section9';
+import Footer from './Footer/Footer';
 
 export default class Home extends Component {
     componentDidMount = () => {
@@ -26,6 +27,7 @@ export default class Home extends Component {
                 <Section7/>
                 <Section8/>
                 <Section9/>
+                <Footer/>
             </div>
         )
     }
